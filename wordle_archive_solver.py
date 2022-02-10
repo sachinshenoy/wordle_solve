141c6cd458cea4031b3173d4d7dfbdbd84bb6cc1
Sachin Shenoy <SachinShenoy76@Gmail.com>
2022-02-09 22:15:21 -0500

    Added Solver for Wordle Archive

--- wordle_archive_solver.py
+++ wordle_archive_solver.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from datetime import date
 import json
 import logging
 import re
@@ -10,9 +12,16 @@ from rich.progress import track
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 start_word = "tizzy"
+archive_number = 7
+dark_mode = False
+start_time = time.time()
+current_word_list = []
+new_word = ""
+
 keyboard = {
-    "q": "div:nth-child(1) > button:nth-child(1)",
+    "q": "div:nth-child(1) > button:nth-child(1) > div",
     "w": "div:nth-child(1) > button:nth-child(2)",
     "e": "div:nth-child(1) > button:nth-child(3)",
     "r": "div:nth-child(1) > button:nth-child(4)",
@@ -22,15 +31,15 @@ keyboard = {
     "i": "div:nth-child(1) > button:nth-child(8)",
     "o": "div:nth-child(1) > button:nth-child(9)",
     "p": "div:nth-child(1) > button:nth-child(10)",
-    "a": "div:nth-child(2) > button:nth-child(2)",
-    "s": "div:nth-child(2) > button:nth-child(3)",
-    "d": "div:nth-child(2) > button:nth-child(4)",
-    "f": "div:nth-child(2) > button:nth-child(5)",
-    "g": "div:nth-child(2) > button:nth-child(6)",
-    "h": "div:nth-child(2) > button:nth-child(7)",
-    "j": "div:nth-child(2) > button:nth-child(8)",
-    "k": "div:nth-child(2) > button:nth-child(9)",
-    "l": "div:nth-child(2) > button:nth-child(10)",
+    "a": "div:nth-child(2) > button:nth-child(1) > div",
+    "s": "div:nth-child(2) > button:nth-child(2)",
+    "d": "div:nth-child(2) > button:nth-child(3)",
+    "f": "div:nth-child(2) > button:nth-child(4)",
+    "g": "div:nth-child(2) > button:nth-child(5)",
+    "h": "div:nth-child(2) > button:nth-child(6)",
+    "j": "div:nth-child(2) > button:nth-child(7)",
+    "k": "div:nth-child(2) > button:nth-child(8)",
+    "l": "div:nth-child(2) > button:nth-child(9)",
     "enter": "div:nth-child(3) > button:nth-child(1)",
     "z": "div:nth-child(3) > button:nth-child(2)",
     "x": "div:nth-child(3) > button:nth-child(3)",
@@ -42,16 +51,43 @@ keyboard = {
     "del": "div:nth-child(3) > button:nth-child(9)",
 }
 
-start_time = time.time()
 
-current_word_list = []
+def sendkeys(web_driver, word):
+    """
+    This function take the webdriver and a string (word) as
+    input which is then entered into the Wordle Puzzle one
+    Charater at a time.
+
+    Args:
+        web_driver (selenium.webdriver): The Keyboard Shadow DOM from Wordle.
+        word (str): Word to be entered into the current row of the wordle puzzle.
+    Returns:
+        None
+    """
+    for char in track(word, description="Entering the Word"):
+        web_driver.find_element(By.CSS_SELECTOR, keyboard[char]).click()
+        time.sleep(0.75)
+    web_driver.find_element(By.CSS_SELECTOR, keyboard["enter"]).click()
 
 
-logging.basicConfig(
-    level=logging.ERROR,
-    format="%(asctime)s %(levelname)s %(message)s",
-    filename="wordle_solve.log",
-)
+def solution_found(result_list):
+    """
+    Get the input dictionary with determination of each character's validity
+    and returns a true/false bool to indicate if the solution has been
+    found.
+
+    Args:
+        result_dict (dict): Dictionary with wordle result for each character in the word.
+    Returns:
+        Bool with True or False to indicate if the wordle solution has been found.
+    """
+    if set(result_list) == {"correct"}:
+        rprint("Congratulations - Solution Found \U0001F44D")
+        rprint(f"Correct Word is {new_word}")
+        rprint(f"Script Execution Time = {time.time() - start_time:.2f} Secs")
+        rprint("Scripted by Sachin Shenoy")
+        rprint("Twitter: https://twitter.com/sachinshenoy")
+    return set(result_list) == {"correct"}
 
 
 def load_words(file_name):
@@ -76,43 +112,6 @@ def load_words(file_name):
     return five_letter_words
 
 
-def solution_found(result_list):
-    """
-    Get the input dictionary with determination of each character's validity
-    and returns a true/false bool to indicate if the solution has been
-    found.
-
-    Args:
-        result_dict (dict): Dictionary with wordle result for each character in the word.
-    Returns:
-        Bool with True or False to indicate if the wordle solution has been found.
-    """
-    if set(result_list) == {"correct"}:
-        rprint("Congratulations - Solution Found \U0001F44D")
-        rprint(f"Script Execution Time = {time.time() - start_time:.2f} Secs")
-        rprint("Scripted by Sachin Shenoy")
-        rprint("Twitter: https://twitter.com/sachinshenoy")
-    return set(result_list) == {"correct"}
-
-
-def sendkeys(web_driver, word):
-    """
-    This function take the webdriver and a string (word) as
-    input which is then entered into the Wordle Puzzle one
-    Charater at a time.
-
-    Args:
-        web_driver (selenium.webdriver): The Keyboard Shadow DOM from Wordle.
-        word (str): Word to be entered into the current row of the wordle puzzle.
-    Returns:
-        None
-    """
-    for char in track(word, description="Entering the Word"):
-        web_driver.find_element(By.CSS_SELECTOR, keyboard[char]).click()
-        time.sleep(0.75)
-    web_driver.find_element(By.CSS_SELECTOR, keyboard["enter"]).click()
-
-
 def find_bg(web_driver, row_number):
     """
     Identifies the background colors of the words from the results grid
@@ -133,29 +132,23 @@ def find_bg(web_driver, row_number):
     """
     char_results = []
     result_list = []
-    game_row_x_root = web_driver.find_element(
-        By.CSS_SELECTOR, f"#board > game-row:nth-child({row_number})"
-    )
-    game_row_x_root_shadow = game_row_x_root.shadow_root
     for i in range(1, 6):
-        game_row_x_i_root = game_row_x_root_shadow.find_element(
-            By.CSS_SELECTOR, f"div > game-tile:nth-child({i})"
+        game_row = web_driver.find_element(
+            By.CSS_SELECTOR, f"span:nth-child({i+(row_number-1)*5})"
         )
-        game_row_x_i_root_shadow = game_row_x_i_root.shadow_root
-        cell = game_row_x_i_root_shadow.find_element(By.CSS_SELECTOR, "div")
-        cell_bg = cell.value_of_css_property("background-color")
+        cell_bg = game_row.value_of_css_property("background-color")
         result_list.append(cell_bg)
         # rprint(cell_bg)
 
     for bg in track(result_list, description="Analyzing Results"):
         match bg:
-            case "rgba(181, 159, 59, 1)" | "rgba(201, 180, 88, 1)":
+            case "rgba(234, 179, 8, 1)" | "rgba(201, 180, 88, 1)":
                 char_results.append("present")
-            case "rgba(58, 58, 60, 1)" | "rgba(120, 124, 126, 1)":
+            case "rgba(96, 102, 133, 1)" | "rgba(120, 124, 126, 1)":
                 char_results.append("absent")
-            case "rgba(83, 141, 78, 1)" | "rgba(106, 170, 100, 1)":
+            case "rgba(99, 170, 85, 1)" | "rgba(106, 170, 100, 1)":
                 char_results.append("correct")
-            case "rgba(129, 131, 132, 1)":
+            case "rgba(54, 57, 74, 1)" | "rgba(231, 232, 238, 1)":
                 char_results.append("other")
             case default:
                 char_results.append("other")
@@ -297,20 +290,31 @@ def solve_row(row_results, word_guess):
 
 def main():
     """
-    Script to control Chrome Browser to Solve that day's
-    Wordle Puzzle.
+    Script to control Chrome Browser to Solve the Wordle Puzzle Archive.
     Input: None
     Output: Solution to the Wordle Puzzle by controlling the browser
     """
 
     global current_word_list
+    global new_word
     current_word_list = load_words("wordle_words.txt")
     allowed_guesses = load_words("wordle_allowed_guesses.txt")
 
+    # Check the Start Word is Valid !!
     if not (start_word in current_word_list or start_word in allowed_guesses) or not (
         len(start_word) == 5
     ):
         rprint("Uh Oh - Please check 'Start Word' \U0001F622")
+        rprint(f"Script Execution Time = {time.time() - start_time: .2f} Secs")
+        rprint("Scripted by Sachin Shenoy")
+        rprint("Twitter: https://twitter.com/sachinshenoy")
+        sys.exit()
+
+    # Check the Archive Number is Valid:
+    current_wordle_number = (date(2022, 2, 9) - date.today()).days + 235
+    rprint((date(2022, 2, 9) - date.today()).days)
+    if not (archive_number <= 0) and (archive_number > current_wordle_number):
+        rprint("Uh Oh - Invalid Archive Number \U0001F622")
         rprint(f"Script Execution Time = {time.time() - start_time: .2f} Secs")
         rprint("Scripted by Sachin Shenoy")
         rprint("Twitter: https://twitter.com/sachinshenoy")
@@ -321,45 +325,59 @@ def main():
     options.add_experimental_option("detach", True)
 
     driver = webdriver.Chrome(options=options)
-    driver.get("https://www.powerlanguage.co.uk/wordle/")
+    driver.get(f"https://www.devangthakkar.com/wordle_archive/?{archive_number}")
     driver.implicitly_wait(5)
 
-    game_app_root = driver.find_element(By.CSS_SELECTOR, "game-app")
-    game_app_root_shadow = game_app_root.shadow_root
-
-    # Close the initial help screen
-    game_modal_root = game_app_root_shadow.find_element(By.CSS_SELECTOR, "game-modal")
-    game_modal_root_shadow = game_modal_root.shadow_root
-    close_button = game_modal_root_shadow.find_element(By.CSS_SELECTOR, "game-icon")
+    #
+    # Close the initial Welcome Screen
+    #
+    close_button = driver.find_element(
+        By.CSS_SELECTOR, "body > div:nth-child(5) > div > div > div > button"
+    )
     close_button.click()
 
-    # Identify the Keyboard Elements in the Shadow DOM
-    game_keyboard_root = game_app_root_shadow.find_element(
-        By.CSS_SELECTOR, "game-keyboard"
-    )
-    game_keyboard_root_shadow = game_keyboard_root.shadow_root
-    game_keyboard_root_shadow.find_element(By.CSS_SELECTOR, "#keyboard")
+    # Turn On Dark Mode
+    if dark_mode:
+        settings_button = driver.find_element(By.CSS_SELECTOR, "button:nth-child(1)")
+        settings_button.click()
+        darkmode_button = driver.find_element(By.CSS_SELECTOR, "#headlessui-switch-3")
+        darkmode_button.click()
+        close_settings = driver.find_element(
+            By.CSS_SELECTOR,
+            "body > div:nth-child(6) > div > div > div > div > div > button",
+        )
+        close_settings.click()
 
-    # Send the start word to the Wordle Puzzle
-    sendkeys(game_keyboard_root_shadow, start_word)
+    # Send the Start Word
+    sendkeys(driver, start_word)
     time.sleep(2)
-    row_results = find_bg(game_app_root_shadow, 1)
+    row_results = find_bg(driver, 1)
+    rprint(row_results)
     new_word = solve_row(row_results, start_word)
     if solution_found(row_results):
+        driver.find_element(
+            By.CSS_SELECTOR,
+            "body > div:nth-child(5) > div > div > div > div > div > button",
+        ).click()
         sys.exit()
 
-    # sendkeys(game_keyboard_root_shadow, "stomp")
+    # sendkeys(driver, "stomp")
     # time.sleep(3)
-    # row_results = find_bg(game_app_root_shadow, 2)
+    # row_results = find_bg(driver, 2)
     # new_word = solve_row(row_results, "stomp")
     # if solution_found(row_results):
     #     sys.exit()
 
     for i in range(2, 7):
-        sendkeys(game_keyboard_root_shadow, new_word)
+        sendkeys(driver, new_word)
         time.sleep(2)
-        row_results = find_bg(game_app_root_shadow, i)
+        row_results = find_bg(driver, i)
+        rprint(row_results)
         if solution_found(row_results):
+            driver.find_element(
+                By.CSS_SELECTOR,
+                "body > div:nth-child(5) > div > div > div > div > div > button",
+            ).click()
             sys.exit()
         else:
             new_word = solve_row(row_results, new_word)
